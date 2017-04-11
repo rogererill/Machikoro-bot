e2c5954a38a2d0cd487c1b1c426cacd18d0ed4e7
Roger Erill <rogerguddy@gmail.com>
2017-04-11 16:48:01 +0200

    Add internal ID to each kind of card

--- src/main/java/com/erill/card/CardName.java
+++ src/main/java/com/erill/card/CardName.java
@@ -5,47 +5,53 @@ package com.erill.card;
  */
 public enum CardName {
 
-    WHEAT_FIELD("Wheat field"),
-    RANCH("Ranch"),
-    FOREST("Forest"),
-    MINE("Mine"),
-    APPLE_ORCHARD("Apple orchard"),
-    BAKERY("Bakery"),
-    CONVENIENCE_STORE("Convenience Store"),
-    CHEESE_FACTORY("Cheese factory"),
-    FURNITURE_FACTORY("Furniture factory"),
-    FRUIT_MARKET("Fruit and vegetables market"),
-    CAFE("Cafe"),
-    RESTAURANT("Family restaurant"),
-    STADIUM("Stadium"),
-    TV_STATION("TV Station"),
-    BUSINESS_CENTER("Business center"),
-    TRAIN_STATION("Train Station"),
-    SHOPPING_MALL("Shopping mall"),
-    AMUSEMENT_PARK("Amusement park"),
-    RADIO_TOWER("Radio tower"),
-    FLOWER_ORCHARD("Flower orchard"),
-    MACKEREL_BOAT("Mackerel boat"),
-    TUNA_BOAT("Tuna boat"),
-    FLOWER_SHOP("Flower shop"),
-    FOOD_WAREHOUSE("Food warehouse"),
-    SUSHI_BAR("Sushi bar"),
-    PIZZA("Pizza joint"),
-    HAMBURGER("Hamburger stand"),
-    PUBLISHER("Publisher"),
-    TAX_OFFICE("Tax office"),
-    CITY_HALL("City hall"),
-    HARBOR("Harbor"),
-    AIRPORT("Airport");
+    WHEAT_FIELD("Wheat field",1),
+    RANCH("Ranch",2),
+    FOREST("Forest",3),
+    MINE("Mine",4),
+    APPLE_ORCHARD("Apple orchard",5),
+    BAKERY("Bakery",6),
+    CONVENIENCE_STORE("Convenience Store",7),
+    CHEESE_FACTORY("Cheese factory",8),
+    FURNITURE_FACTORY("Furniture factory",9),
+    FRUIT_MARKET("Fruit and vegetables market",10),
+    CAFE("Cafe",11),
+    RESTAURANT("Family restaurant",12),
+    STADIUM("Stadium",13),
+    TV_STATION("TV Station",14),
+    BUSINESS_CENTER("Business center",15),
+    TRAIN_STATION("Train Station",16),
+    SHOPPING_MALL("Shopping mall",17),
+    AMUSEMENT_PARK("Amusement park",18),
+    RADIO_TOWER("Radio tower",19),
+    FLOWER_ORCHARD("Flower orchard",20),
+    MACKEREL_BOAT("Mackerel boat",21),
+    TUNA_BOAT("Tuna boat",22),
+    FLOWER_SHOP("Flower shop",23),
+    FOOD_WAREHOUSE("Food warehouse",24),
+    SUSHI_BAR("Sushi bar",25),
+    PIZZA("Pizza joint",26),
+    HAMBURGER("Hamburger stand",27),
+    PUBLISHER("Publisher",28),
+    TAX_OFFICE("Tax office",29),
+    CITY_HALL("City hall",30),
+    HARBOR("Harbor",31),
+    AIRPORT("Airport",32);
 
 
     private String name;
+    private int cardId;
 
-    CardName(String name) {
+    CardName(String name, int cardId) {
         this.name = name;
+        this.cardId = cardId;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCardId() {
+        return cardId;
     }
 }
